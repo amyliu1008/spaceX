81ad24906d4b041ac1cf5ef1d43935d7b546522d
JLRIEU1\yliu78 <yliu78@jaguarlandrover.com>
2024-11-20 17:23:45 +0000

    restructure main file

--- spacex/main.py
+++ spacex/main.py
@@ -1,18 +1,26 @@
 from space import SpaceX
 import pandas as pd
 
-spacex = SpaceX()
 
-years = spacex.launches["date_utc"].dt.year.unique().tolist()
+def main():
+    spacex = SpaceX()
 
-launch_for_year = spacex.get_launch_for_year(years)
-launch_for_year_df = pd.DataFrame(
-    launch_for_year.items(), columns=["Year", "Number of Launches"]
-)
+    years = spacex.launches["date_utc"].dt.year.unique().tolist()
 
-print(launch_for_year_df)
+    launch_for_year = spacex.get_launch_for_year(years)
+    launch_for_year_df = pd.DataFrame(
+        launch_for_year.items(), columns=["Year", "Number of Launches"]
+    )
 
-sites = spacex.launchpads["full_name"].tolist()
-launch_for_site = spacex.get_launch_for_site(sites)
-launch_for_site_df = pd.DataFrame.from_dict(launch_for_site, orient="index")
-print(launch_for_site_df)
+    print(launch_for_year_df)
+
+    sites = spacex.launchpads["full_name"].tolist()
+    launch_for_site = spacex.get_launch_for_site(sites)
+    launch_for_site_df = pd.DataFrame.from_dict(
+        launch_for_site, orient="index"
+    )
+    print(launch_for_site_df)
+
+
+if __name__ == "__main__":
+    main()
